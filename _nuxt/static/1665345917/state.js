e409fc1e41150a460c64c98454716cb8bcfa9812
fermeaux <fermeaux@users.noreply.github.com>
2022-10-09 21:09:27 +0000

    deploy: 07d58fd89978c4d5441d3afd53297dee1e7e2b04

--- _nuxt/static/1665345917/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwedding\u002F_nuxt\u002Fstatic\u002F1665345917",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fwedding\u002F",assetsPath:"\u002Fwedding\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"065b4dbb"}}}}(null));
